f6e75f7b417adc1f09527923d51f89cb63e3d664
MissxR0 <roplay112@gmail.com>
2025-04-23 08:50:14 +0200

    print error message for all commands

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 
 int main() {
+
   // Flush after every std::cout / std:cerr
   // Activa el modo "unitbuf" (unbuffered) para el stdout y stderr
   // Imprimirá inmediatamente sin esperar a que el bufer se llene (no hay que poner endl)
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
   std::cout << "$ ";
 
+  // Declaro la variable "input" que será una string
   std::string input;
+  // Guardo lo que ingrese el usuario en "input"
   std::getline(std::cin, input);
+
+  // No finalizo con endl porque tenemos el modo ubitbuf activado, solo hago un salto de línea
+  std::cout << input << ": command not found\n";
 }
